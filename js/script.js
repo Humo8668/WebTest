90c341970fa3c7ea7be39588a171a1c20abc0d3d
Humo8668 <56265395+Humo8668@users.noreply.github.com>
2021-03-24 17:43:29 +0500

    End of 24032021

--- js/script.js
+++ js/script.js
@@ -1,9 +1,15 @@
+const METHOD_GET = "GET";
+const METHOD_POST = "POST";
+const METHOD_PUT = "PUT";
+const METHOD_DELETE = "DELETE";
+
+
 
 function hideDOM(DOM)
 {
     if(DOM == null || DOM.length == 0)
         return;
-    
+
     if(typeof DOM == "string")
     {
         var node = document.getElementById(DOM);
@@ -17,8 +23,8 @@ function hideDOM(DOM)
         if(DOM.nodeType == null || DOM.nodeType != 1) // DOM.nodeType != ELEMENT_NODE
         return;
 
-        DOM.style.display = 'none';    
-    } 
+        DOM.style.display = 'none';
+    }
     return;
 }
 
@@ -26,7 +32,7 @@ function showDOM(DOM)
 {
     if(DOM == null || DOM.length == 0)
         return;
-    
+
     if(typeof DOM == "string")
     {
         var node = document.getElementById(DOM);
@@ -40,17 +46,50 @@ function showDOM(DOM)
         if(DOM.nodeType == null || DOM.nodeType != 1) // DOM.nodeType != ELEMENT_NODE
         return;
 
-        DOM.style.display = 'block';    
-    } 
+        DOM.style.display = 'block';
+    }
     return;
 }
 
+function showBodyBlock(blockToShow)
+{
+    var blocks = $("#body_type_blocks")[0].children;
 
+    if(blocks != null && blocks.length != 0)
+        for(var i = 0; i < blocks.length; i++)
+            hideDOM(blocks[i]);
 
+    if(blockToShow != null)
+        showDOM(blockToShow);
+}
 
+function getHeaderAsObject()
+{
+    var headers = {};
+    $("#headers-tab").each(function(i, elem){
+        var key = $(":input[name='headers_key']", elem).val();
+        var value = $(":input[name='headers_value']", elem).val();
+        if (key.length == 0)
+            return; // continue;
+            //return false; // break;
+        headers[key] = value;
+    });
+    return headers;
+}
 
-
-
+function getParamsAsObject()
+{
+    var params = {};
+    $("#params-tab").each(function(i, elem){
+        var key = $(":input[name='params_key']", elem).val();
+        var value = $(":input[name='headers_value']", elem).val();
+        if (key.length == 0)
+            return; // continue;
+            //return false; // break;
+        params[key] = value;
+    });
+    return headers;
+}
 
 $(document).on('click', '.btn-add', function(e)
 {
@@ -87,11 +126,58 @@ $(".field_type").change(function () {
 $("input[type='radio'][name='content-type']").change(function (e) {
     var blockToShow = $('#' + e.target.value)[0];
     var blocks = $("#body_type_blocks")[0].children;
-    
-    if(blocks != null && blocks.length != 0)
-        for(var i = 0; i < blocks.length; i++) 
-            hideDOM(blocks[i]);
 
-    if(blockToShow != null)  
-        showDOM(blockToShow);
-})
+    showBodyBlock(blockToShow);
+});
+
+$("#send_request").click(function () {
+    var url = $("#URL").val();
+    var contentType = $('input[name=content-type]:checked').val();
+    var method = $("#method").val();
+    var ajax_obj = {};
+
+
+    //alert(url + " " + contentType + " " + method);
+    if (url == null || url == undefined || url.length == 0)
+        return;
+
+    if (contentType == "body_none" || contentType.length == 0)
+        contentType = false;
+
+    ajax_obj['url'] = url;
+    ajax_obj['method'] = method;
+    if(method == METHOD_GET)
+    {
+        ajax_obj['data'] = getParamsAsObject();
+    }
+    else
+    {
+        ajax_obj['contentType'] = contentType;
+        ajax_obj['data'] = "";
+        ajax_obj['headers'] = "";
+    }
+
+
+    /*if(method == METHOD_GET)
+    {
+        $.ajax({
+            url: url,
+            method: method,
+            data: getParamsAsObject(),
+            contentType: contentType,
+
+        });
+    }
+    else
+    {
+        $.ajax({
+            url: url,
+            method: method,
+            headers: getHeaderAsObject(),
+            contentType: contentType,
+
+        });
+    }*/
+
+
+});
