122c722bda20c97105672d6c9b93095b1c0d5578
Humo8668 <56265395+Humo8668@users.noreply.github.com>
2021-03-24 15:10:10 +0500

    before 24032021

--- js/script.js
+++ js/script.js
@@ -1,4 +1,57 @@
 
+function hideDOM(DOM)
+{
+    if(DOM == null || DOM.length == 0)
+        return;
+    
+    if(typeof DOM == "string")
+    {
+        var node = document.getElementById(DOM);
+        if(node == null)
+            return;
+
+        node.style.display = 'none';
+    }
+    else if(typeof DOM == 'object')
+    {
+        if(DOM.nodeType == null || DOM.nodeType != 1) // DOM.nodeType != ELEMENT_NODE
+        return;
+
+        DOM.style.display = 'none';    
+    } 
+    return;
+}
+
+function showDOM(DOM)
+{
+    if(DOM == null || DOM.length == 0)
+        return;
+    
+    if(typeof DOM == "string")
+    {
+        var node = document.getElementById(DOM);
+        if(node == null)
+            return;
+
+        node.style.display = 'block';
+    }
+    else if(typeof DOM == 'object')
+    {
+        if(DOM.nodeType == null || DOM.nodeType != 1) // DOM.nodeType != ELEMENT_NODE
+        return;
+
+        DOM.style.display = 'block';    
+    } 
+    return;
+}
+
+
+
+
+
+
+
+
 $(document).on('click', '.btn-add', function(e)
 {
     e.preventDefault();
@@ -27,9 +80,18 @@ $(".field_type").change(function () {
     if(value_input.length == 0)
         return;
 
+    value_input.val(null);
     value_input.attr("type", field_type);
 });
 
 $("input[type='radio'][name='content-type']").change(function (e) {
-    console.log(e.target);
+    var blockToShow = $('#' + e.target.value)[0];
+    var blocks = $("#body_type_blocks")[0].children;
+    
+    if(blocks != null && blocks.length != 0)
+        for(var i = 0; i < blocks.length; i++) 
+            hideDOM(blocks[i]);
+
+    if(blockToShow != null)  
+        showDOM(blockToShow);
 })
